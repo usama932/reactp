dd99edbf046c6ed4eee8255d0fe746249af02d17
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-06-06 05:55:33 -0700

    push counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Hello from './components/Hello';
-import Profile from './components/Profile';
-import Message from './components/Message';
+
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
     
-      <Message></Message>
+      <Counter></Counter>
      
     </div>
   );
