595d11f41123495fbdd43d710742f4d28890b847
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-06-07 05:45:19 -0700

    push index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React  from 'react';
 
-import Counter from './components/Counter';
-
+import {Routes, Route,Link} from 'react-router-dom';
+import List from './components/List';
+import Create from './components/Create';
 function App() {
   return (
-    <div className="App">
-    
-      <Counter></Counter>
-     
+    <div className="container mt-5">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand"  to={'/'}>Test</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+            <li className="nav-item active">
+                <Link className="nav-link" to={'/list'}>List </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to={'/create'}>Create</Link>
+            </li>
+           
+        
+            </ul>
+        </div>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/list" element={<List />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+
+      </div>
     </div>
   );
 }
--- src/components/Profile.js
+++
@@ -1,7 +0,0 @@
-function Profile(props){
-   // console.log(props)
-    return <h1>Profile {props.name} {props.last_name}
-    {props.children}</h1>
-}
-
-export default Profile;
