0b3904414047c816165c02e519b554be2195f7f1
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-06-12 01:01:20 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from  './components/Hello.js';
 function App() {
   return (
     <div className="App">
+      <Hello></Hello>
       <h1>Hello</h1>
     </div>
   );
---
+++ src/components/Hello.js
@@ -0,0 +1,4 @@
+function Hello(){
+    return <h1>Hello Umme Kalssom</h1>
+}
+export default Hello;
